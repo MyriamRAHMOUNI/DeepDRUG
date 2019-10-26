86dea9803700d628f377e22a7dd97debd65bbaa0
MyriamRAHMOUNI <54821238+MyriamRAHMOUNI@users.noreply.github.com>
2019-10-26 16:07:42 +0200

    ajout-modification-diderot

--- preparation-donnees.py
+++ preparation-donnees.py
@@ -16,40 +16,38 @@ random.shuffle(files)
 targets = []
 features = []
 steroides = []
-controls = []
 
-for filename in files[:200]:
+for filename in files:
     protein_name = filename[22:-4]
     if protein_name in nuc:
         features.append(np.load(filename))
-        target = [1, 0]
+        target = [1, 0, 0]
         targets.append(target)
     elif protein_name in hemes:
         features.append(np.load(filename))
-        target = [0, 1]
+        target = [0, 1, 0]
+        targets.append(target)
+    elif protein_name in control:
+        con = np.load(filename)
+        features.append(np.reshape(con,(14, 32, 32, 32)))
+        target = [0, 0, 1]
         targets.append(target)
     elif protein_name in steroid:
         ster = np.load(filename)
         steroides.append(np.reshape(ster,(14, 32, 32, 32)))
-    elif protein_name in control:
-        con = np.load(filename)
-        controls.append(np.reshape(con, (14, 32, 32, 32)))
     else:
         print (protein_name)
         print ('I can t find this proteine in the lists')
         break
 features = np.array(features)
 targets = np.array(targets)
-controls = np.array(controls)
 steroides = np.array(steroides)
 
 print('features: ', features.shape)
 print('targets: ', targets.shape)
-print('control:', controls.shape)
 print('stero: ', steroides.shape)
 
 np.save('features', features)
 np.save('targets', targets)
-np.save('controls', controls)
 np.save('steroides', steroides)
 
